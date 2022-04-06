482aa85ab5a47c5fd3435e8a4b4272bda5295db5
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2022-04-05 23:14:21 -0500

    Correctly merges chava_branch and the character movement/interaction branch
    It works... but now wot.
    
    Need to merge it to the main branch and get the updates made to the memory game after this point.

--- Videojuegos/PercussionIslands/Assets/Scripts/MemoryGameController.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/MemoryGameController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour
+public class MemoryGameController : MonoBehaviour
 {
     [SerializeField]
     private Sprite bgImage;
