4a15de47ebd91dd95234d8fc8b3f21f93a500f42
A01352283 <a01352283@itesm.mx>
2022-04-06 08:16:18 -0500

    Matches the changes from the memory game game over screen commit

--- Videojuegos/PercussionIslands/Assets/Scripts/MemoryGameController.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/MemoryGameController.cs
@@ -7,8 +7,9 @@ public class MemoryGameController : MonoBehaviour
 {
     [SerializeField]
     private Sprite bgImage;
-
     private AudioSource audio_s;
+    public AudioClip corr;
+    public AudioClip incorr;
     public Sprite image_d;
     public Sprite[] puzzles;
     public AudioClip[] sounds;
@@ -30,6 +31,7 @@ public class MemoryGameController : MonoBehaviour
     private int firstGuessIndex, secondGuessIndex;
     private int firstGuessPuzzle, secondGuessPuzzle;
     public Text count_guess;
+    public Text inst_name;
 
     public MemoryGameOverScreen MemoryGameOverScreen;
     public void GameOver(){
@@ -85,6 +87,7 @@ public class MemoryGameController : MonoBehaviour
             }
             else{
                 btns[firstGuessIndex].image.sprite= image_d;
+                inst_name.text="Sound Card";
                 audio_s.clip= cards[firstGuessIndex].snd;
                 audio_s.Play();
             }
@@ -120,7 +123,6 @@ public class MemoryGameController : MonoBehaviour
             yield return new WaitForSeconds(.8f);
             btns[firstGuessIndex].interactable = false;
             btns[secondGuessIndex].interactable = false;
-
             btns[firstGuessIndex].image.color = new Color(0,0,0,0);
             btns[secondGuessIndex].image.color = new Color(0,0,0,0);
 
