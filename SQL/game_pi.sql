5523b30935a609467abd7e67d531789391f1cc90
Salvador Salgado <chavasalgado27@gmail.com>
2022-04-18 14:55:04 -0500

    corregir sql

--- SQL/game_pi.sql
+++ SQL/game_pi.sql
@@ -11,16 +11,6 @@ CREATE TABLE game_user (
   KEY idx_user_email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table progress (
-	progress_id smallint unsigned not null auto_increment,
-    sections_id smallint unsigned not null,
-    inventory_id smallint unsigned not null,
-    score_id smallint unsigned not null,
-    primary key (progress_id),
-    CONSTRAINT `fk_progress_sections` FOREIGN KEY (island_id) REFERENCES sections (sections_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_progress_inventory` FOREIGN KEY (inventory_id) REFERENCES inventory (inventory_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_progress_score` FOREIGN KEY (score_id) REFERENCES score (score_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 create table inventory (
 	inventory_id smallint unsigned not null auto_increment,
@@ -31,16 +21,14 @@ create table inventory (
     primary key (inventory_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table sections (
-	sections_id smallint unsigned not null auto_increment,
-    hub_id smallint unsigned not null,
-    island1_id smallint unsigned not null,
-    island2_id smallint unsigned not null,
-    primary key (sections_id),
-    CONSTRAINT `fk_sections_hub` FOREIGN KEY (hub_id) REFERENCES hub (hub_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_sections_island1` FOREIGN KEY (island1_id) REFERENCES island1 (island1_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_sections_island2` FOREIGN KEY (island2_id) REFERENCES island2 (island2_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)	ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table hub (
+	hub_id smallint unsigned not null auto_increment,
+    xilophone boolean not null,
+    marimba boolean not null,
+    bongo boolean not null,
+    primary key (hub_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 create table questions (
 	question_id smallint unsigned not null auto_increment,
@@ -52,26 +40,6 @@ create table questions (
     primary key (question_id)
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table scores (
-	score_id smallint unsigned not null auto_increment,
-    memory_id smallint not null,
-    trivia_id smallint not null,
-    rhythm_id smallint not null,
-    memorysounds_id smallint not null,
-    primary key (score_id),
-    CONSTRAINT `fk_scores_memory` FOREIGN KEY (memory_id) REFERENCES memory_game (memory_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_scores_trivia` FOREIGN KEY (trivia_id) REFERENCES trivia_game (trivia_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_scores_rhythm` FOREIGN KEY (rhythm_id) REFERENCES rhythm_game (rhythm_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT `fk_scores_memorysounds` FOREIGN KEY (memorysounds_id) REFERENCES memorysounds_game (memorysounds_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-create table hub (
-	hub_id smallint unsigned not null auto_increment,
-    xilophone boolean not null,
-    marimba boolean not null,
-    bongo boolean not null,
-    primary key (hub_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 create table island1(
 	island1_id smallint unsigned not null auto_increment,
@@ -88,6 +56,18 @@ create table island2(
     box boolean not null,
     primary key (island2_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+create table sections (
+	sections_id smallint unsigned not null auto_increment,
+    hub_id smallint unsigned not null,
+    island1_id smallint unsigned not null,
+    island2_id smallint unsigned not null,
+    primary key (sections_id),
+    CONSTRAINT `fk_sections_hub` FOREIGN KEY (hub_id) REFERENCES hub (hub_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_sections_island1` FOREIGN KEY (island1_id) REFERENCES island1 (island1_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_sections_island2` FOREIGN KEY (island2_id) REFERENCES island2 (island2_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)	ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 
 create table memory_game (
@@ -122,10 +102,27 @@ create table trivia_game (
     primary key (trivia_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table rhythm_game (
-	rhythm_id smallint unsigned not null auto_increment,
-    best_score smallint not null,
-	time_played time,
-    last_score smallint,
-    primary key (rhythm_id)
+
+create table scores (
+	score_id smallint unsigned not null auto_increment,
+    memory_id smallint unsigned not null,
+    trivia_id smallint unsigned not null,
+    rhythm_id smallint unsigned not null,
+    memorysound_id smallint unsigned not null,
+    primary key (score_id),
+    CONSTRAINT `fk_scores_memory` FOREIGN KEY (memory_id) REFERENCES memory_game (memory_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_scores_trivia` FOREIGN KEY (trivia_id) REFERENCES trivia_game (trivia_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_scores_rhythm` FOREIGN KEY (rhythm_id) REFERENCES rhythm_game (rhythm_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_scores_memorysounds` FOREIGN KEY (memorysound_id) REFERENCES memorysounds_game (memorysound_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+create table progress (
+	progress_id smallint unsigned not null auto_increment,
+    sections_id smallint unsigned not null,
+    inventory_id smallint unsigned not null,
+    score_id smallint unsigned not null,
+    primary key (progress_id),
+    CONSTRAINT `fk_progress_sections` FOREIGN KEY (sections_id) REFERENCES sections (sections_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_progress_inventory` FOREIGN KEY (inventory_id) REFERENCES inventory (inventory_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_progress_score` FOREIGN KEY (score_id) REFERENCES scores (score_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
