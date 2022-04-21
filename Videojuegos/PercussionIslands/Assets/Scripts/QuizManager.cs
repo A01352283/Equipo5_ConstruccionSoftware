d7a5a7ef090550641a933686b1663ab8e90157ea
Salvador Salgado <chavasalgado27@gmail.com>
2022-04-20 22:13:15 -0500

    Update trivia

--- Videojuegos/PercussionIslands/Assets/Scripts/QuizManager.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/QuizManager.cs
@@ -15,6 +15,8 @@ public class QuizManager : MonoBehaviour
 
     private QuestionAnswers currentQuestion;
     private int index =0;
+    private int mult= 10;
+    private int score= 0;
 
     private void Start(){
         generateQuestions();
@@ -23,12 +25,16 @@ public class QuizManager : MonoBehaviour
     }
 
     public void showQuestions(){
+        while(index<QnA.Count){
         currentQuestion=QnA[index];
+        Shuffle(currentQuestion.answers);
         QuestionText.text=currentQuestion.question;
         Answer1Text.text=currentQuestion.answers[0];
         Answer2Text.text=currentQuestion.answers[1];
         Answer3Text.text=currentQuestion.answers[2];
         Answer4Text.text=currentQuestion.answers[3];
+        }
+        
     }
 
     public void checkAns(int ans_index){
@@ -39,6 +45,7 @@ public class QuizManager : MonoBehaviour
         }
         else{
             index++;
+            Debug.Log("INCORRECT");
             showQuestions();
         }
     }
@@ -56,10 +63,21 @@ public class QuizManager : MonoBehaviour
         }
     }
     */
+    //We will latter generate the questions by json files
     void generateQuestions(){
         QnA.Add(new QuestionAnswers("How old are you?","22","15","75","30"));
         QnA.Add(new QuestionAnswers("Best game?","Percussion Island","Pokemon","Cuphead","TroyWars"));
         QnA.Add(new QuestionAnswers("Day of the week?","Friday","Tuesday","Thursday","Sunday"));
         QnA.Add(new QuestionAnswers("Best School?","Tec","UNAM","ANAHUAC","POLI"));
+    }
+
+    //Function that allows to shuffle the order of the answers
+    void Shuffle(List<string> s_list){
+        for (int i = 0; i < s_list.Count; i++){
+            string temp = s_list[i];
+            int randomIndex= Random.Range(i, s_list.Count);
+            s_list[i]=s_list[randomIndex];   
+            s_list[randomIndex]= temp;     
+        }
     }
 }
