d2ab858088e57fd3ec689b44dc9c3685905f4834
Salvador Salgado <chavasalgado27@gmail.com>
2022-04-22 13:59:58 -0500

    Comments

--- Videojuegos/PercussionIslands/Assets/Scripts/Card.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/Card.cs
@@ -1,3 +1,4 @@
+//This Script contains
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Videojuegos/PercussionIslands/Assets/Scripts/QuestionAnswers.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/QuestionAnswers.cs
@@ -1,15 +1,17 @@
-
+//This script contains the class QuestioAnswer that contains each question with its respective answers
 using System.Collections;
 using System.Collections.Generic;
 [System.Serializable]
 
 public class QuestionAnswers
 {
+    public int question_id;
     public string question;
     public List<string> answers;
     public string correctanswer;
 
-    public QuestionAnswers(string _question,string _ans1, string _ans2, string _ans3, string _ans4){
+    public QuestionAnswers(int _questionid, string _question,string _ans1, string _ans2, string _ans3, string _ans4){
+            this.question_id=_questionid;
             this.answers=new List<string>();
             this.question = _question;
             this.answers.Add(_ans1);
