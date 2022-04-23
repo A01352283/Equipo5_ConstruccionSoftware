4a7f4a8df816e2a77dadc22191ffa58952b8a73b
ivalani amador <ivalanipiaga@gmail.com>
2022-04-22 20:57:48 -0500

    Comments of memorySoundg_game

--- Videojuegos/PercussionIslands/Assets/Scripts/MemorySounds/MemoryS_Manager.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/MemorySounds/MemoryS_Manager.cs
@@ -5,93 +5,98 @@ using UnityEngine.UI;
 
 public class MemoryS_Manager : MonoBehaviour
 {
-    public Image PreviewImage; //VorschauImage -- instrument display (top of the board or "insrument view") 
-    public GameObject DontTouch; // gameobject that defines when an instrument square should be touched or not.
-    // dont touch image (block instruments or "unTouch") -- Nichtdrüken imagen
-    public List<Color> InstrumentColor; //Farben
-    public int AllColors; //varios colores -AnzahlFaber -- 
-    public int ColorMix;// espectaculo de colores - FarbenZeigen 
-    public int Nochfehlend; //no falta? 
-    public Text NochfelendText; // sigue esperando
-    public List<int> Reihenfolge;
-    public Text ScoreText; //LevelText
-    public GameObject Game_over;  //EndScene 
-    public int HigthScore;
-    public Text HigthScore_text;
+    public Image PreviewImage; // Display in the top of board (instrument color previw) 
+    public GameObject DontTouch; // gameobject that defines when an instrument square should be touched or not (touch lock)
+    public List<Color> InstrumentColor; //List of colors that individually represent each instrument
+    public int AllColors; //Specifies the number of colors that should be displayed in the Preview image (Increase the amount)
+    public int ColorMix;//Start mixing colors from the amount designated by "AllColors"
+    public int InstrumentCount; //Number of instruments that are still missing to finish the rhythm (Inverted count)
+    public Text InstrumentCount_Text; // Shows in the "preview Image" how many clicks the player needs to complete the round
+    public List<int> ColorOrdenInPreview; // Color order shown in "Preview image"
+    public Text ScoreText;  
+    public GameObject Game_over;  //When losing in the game a Game Over screen is shown
+    public int HighScore; // Stores the highest score in the game
+    public Text HighScore_text; // Show the highest score
+    public Text LastScore_text; //The same of ScoreText
     
+
+    //When you start a game, the color list is reset and the color count begins
     void Start()
     {
-       Reihenfolge = new List<int>();
+       ColorOrdenInPreview = new List<int>();
        StartCoroutine(Starten()); 
     }
 
+
+    // Generate the colors and save the score list
     public void Generator()
     {
-        AllColors++;
-        ScoreText.text = "Score:  " + AllColors;
-        Reihenfolge.Add(Random.Range(0, 4)); 
+        AllColors++; // Round counter  
+        ScoreText.text = "Score:  " + AllColors; // Show the score on the screen 
+        LastScore_text.text = "Your Score: " + AllColors; // On the game over screen, it shows the score you had while playing
+        ColorOrdenInPreview.Add(Random.Range(0, 4)); // Generates the order of the colors randomly, in a range of 4 (4 instruments)
 
         ShowValues();
     }
 
-    public void ShowValues() //mostrar valores - Vorschau Zeigen 
+    public void ShowValues() 
     {
-        if(Reihenfolge.Count <= ColorMix)
+        if(ColorOrdenInPreview.Count <= ColorMix) //
         {
-            PreviewImage.color = Color.white; 
-            ColorMix = 0; 
-            Nochfehlend= Reihenfolge.Count;
-            NochfelendText.text = NochfelendText.ToString();
-            DontTouch.SetActive(false);
+            PreviewImage.color = Color.white; //when no color is displayed, the screen is blank 
+            ColorMix = 0; // start in 0 
+            InstrumentCount= ColorOrdenInPreview.Count; //Count stabilization
+            InstrumentCount_Text.text = InstrumentCount_Text.ToString();
+            DontTouch.SetActive(false); // block touch 
         }
         else
         {
-            PreviewImage.color = InstrumentColor[Reihenfolge[ColorMix]];
-            StartCoroutine(NachsteZeigen()); //nachste Zeigen 
+            PreviewImage.color = InstrumentColor[ColorOrdenInPreview[ColorMix]];
+            StartCoroutine(ShowNext()); 
         }
     }
 
-//FarbenButton -- Instrument button (farben - color) -- to reference 
+
     public void FarbenButton(int ID)
     {
-        if(ID == Reihenfolge[ColorMix])
+        if(ID == ColorOrdenInPreview[ColorMix]) 
         {
             ColorMix++;
-            Nochfehlend--;
-            NochfelendText.text = Nochfehlend.ToString();
+            InstrumentCount--;
+            InstrumentCount_Text.text = InstrumentCount.ToString();
 
-            if(ColorMix == Reihenfolge.Count)
+            if(ColorMix == ColorOrdenInPreview.Count) //As long as the game is not lost, the counters will increase
             {
                 DontTouch.SetActive(true);
-                NochfelendText.text = "";
-                Nochfehlend = 0;
+                InstrumentCount_Text.text = " ";
+                InstrumentCount = 0;
                 ColorMix = 0;
                 StartCoroutine(Starten());
             }
         }
-        else 
+        else // Upon losing, the game over screen will be displayed along with the other data and buttons.
         {
             Game_over.SetActive(true);
-            DontTouch.SetActive(true);
-            HigthScore = PlayerPrefs.GetInt("Higthscore");
+            DontTouch.SetActive(true); // Screen lock 
+            HighScore = PlayerPrefs.GetInt("High Score");  //shows the stored score
 
-            if(AllColors > HigthScore)
+            if(AllColors > HighScore)
             {
-                HigthScore = AllColors;
-                PlayerPrefs.SetInt("HigthScore", HigthScore);
+                HighScore = AllColors;
+                PlayerPrefs.SetInt("High Score", HighScore);
             }
 
-            HigthScore_text.text = "HigthScore" + HigthScore; 
-            NochfelendText.text = " "; 
-            Nochfehlend = 0;
+            HighScore_text.text = "High Score: " + HighScore; 
+            InstrumentCount_Text.text = " "; 
+            InstrumentCount = 0;
             ColorMix = 0;
         }
     }
 
-    public void Nochmal()
+    public void Nochmal() // Game restart after a game over (play again)
     {
-        Reihenfolge = new List<int>();
-        AllColors = 0;
+        ColorOrdenInPreview = new List<int>(); // restart color orden in preview 
+        AllColors = 0; // start in 0 
         ScoreText.text ="Score: " + AllColors;
         Game_over.SetActive(false);
         StartCoroutine(Starten());
@@ -99,13 +104,13 @@ public class MemoryS_Manager : MonoBehaviour
 
     IEnumerator Starten()
     {
-        yield return new WaitForSeconds(0.5f);
-        Generator();
+        yield return new WaitForSeconds(0.5f); // Time before starting the game
+        Generator(); // Call the generator to start the game 
     }
 
-    IEnumerator NachsteZeigen()
+    IEnumerator ShowNext()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.3f);  //Time between each color displayed
         PreviewImage.color = Color.white;
         yield return new WaitForSeconds(0.7f);
         ColorMix++;
