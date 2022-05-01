618afec39c235050cb3ce14072eac1616673255a
Ivan Rodriguez <xxivanxxy@gmail.com>
2022-05-01 18:11:41 -0500

    GameOverStyleMenuRhythmGame

---
+++ Videojuegos/PercussionIslands/Assets/Scripts/RhythmGame/GameOver.cs
@@ -0,0 +1,26 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        Invoke(FinishGame, 72f);
+    }
+
+    void FinishGame()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
