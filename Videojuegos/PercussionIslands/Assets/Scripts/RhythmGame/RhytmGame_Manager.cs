aa23bc8b85ed0196f9e071f6834ca596e82c05ec
Ivan Rodriguez <xxivanxxy@gmail.com>
2022-05-05 17:03:13 -0500

    More comments

--- Videojuegos/PercussionIslands/Assets/Scripts/RhythmGame/RhytmGame_Manager.cs
+++ Videojuegos/PercussionIslands/Assets/Scripts/RhythmGame/RhytmGame_Manager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Unused script
 public class RhytmGame_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
